9ae32353caa69292ba86f32e695f8939dbea9c75
zalex <mabutamail@gmail.com>
2020-12-20 13:44:35 +0300

    init

---
+++ app.js
@@ -0,0 +1,36 @@
+//  alert("hello")
+
+//  1   Переменные
+// var name = "Zalex"   уже не используется
+// const firstName = 'Zalex'
+// const isProgrammer = true
+// let age = 33    //  можно менять
+//
+// console.log('слоны пошли на север') //  шрифты на русском
+// console.log(age)
+// console.log(isProgrammer)
+
+//  2   мутирование или динамическая типизация
+//const lastName = prompt('Введите значение')
+//alert(firstName + " " + lastName)
+
+//  3   операторы
+// const a = 20
+// const b = 11
+// let currentYear = 2020
+// console.log("a=" + a)
+// console.log("b=" + b)
+// console.log("a-b=" + (a - b))
+// console.log("a+b=" + (a + b))
+// console.log("a/b=" + (a / b))
+// console.log("a*b=" + (a * b))
+// console.log(currentYear++)
+// console.log(currentYear)
+
+//  4   типы данных - всего 8
+const isProgrammer = true
+const name = "Zalex"
+const age = 33
+console.log(typeof isProgrammer)
+console.log(typeof name)
+console.log(typeof age)
